58e0f73d8e9f43a769ae63d4b2d4feb14fbd2394
Strix-Vyxlor <strix.vyxlor@gmail.com>
2024-07-22 13:44:17 +0200

    added superfile floaterm

--- lua/strix/plugins/floaterm.lua
+++ lua/strix/plugins/floaterm.lua
@@ -4,5 +4,6 @@ return {
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>lt", "<CMD>FloatermNew --autoclose=2 --width=0.9 --height=0.9 zsh<CR>", { desc = "Open terminal" })
+    keymap.set("n", "<leader>lf", "<CMD>FloatermNew --autoclose=2 --width=0.9 --height=0.9 spf<CR>", { desc = "Open superfile" })
   end
 }
