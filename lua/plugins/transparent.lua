96b66f73bdf4d828c3c0702fc7dfa3f87cafde49
Strix Vyxlor <strix.vyxlor@gmail.com>
2024-11-01 21:38:37 +0100

    keymap

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,5 +1,9 @@
 return {
 	"xiyaowong/transparent.nvim",
 	lazy = false,
-	config = function() end,
+	config = function()
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>tt", "<cmd>TransparentToggle<CR>", { desc = "toggle transparency" })
+	end,
 }
