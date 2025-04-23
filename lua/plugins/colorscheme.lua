9d004351e7cb7688c0923857459e01228f7a28c3
Strix Vyxlor <strix.vyxlor@gmail.com>
2025-04-23 11:01:27 +0200

    better shell selection

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,30 +6,28 @@ return {
 		local colorscheme = require("base16-colorscheme")
 		local json = require("lunajson")
 
-		local theme = {}
+		local theme = {
+			base00 = "#1e1e2e",
+			base01 = "#181825",
+			base02 = "#313244",
+			base03 = "#45475a",
+			base04 = "#585b70",
+			base05 = "#cdd6f4",
+			base06 = "#f5e0dc",
+			base07 = "#b4befe",
+			base08 = "#f38ba8",
+			base09 = "#fab387",
+			base0A = "#f9e2af",
+			base0B = "#a6e3a1",
+			base0C = "#94e2d5",
+			base0D = "#89b4fa",
+			base0E = "#cba6f7",
+			base0F = "#f2cdcd",
+		}
 
 		local config_dir = os.getenv("XDG_CONFIG_HOME")
 		local file = io.open(config_dir .. "/stylix/palette.json", "r")
-		if not file then
-			theme = {
-				base00 = "#1e1e2e",
-				base01 = "#181825",
-				base02 = "#313244",
-				base03 = "#45475a",
-				base04 = "#585b70",
-				base05 = "#cdd6f4",
-				base06 = "#f5e0dc",
-				base07 = "#b4befe",
-				base08 = "#f38ba8",
-				base09 = "#fab387",
-				base0A = "#f9e2af",
-				base0B = "#a6e3a1",
-				base0C = "#94e2d5",
-				base0D = "#89b4fa",
-				base0E = "#cba6f7",
-				base0F = "#f2cdcd",
-			}
-		else
+		if file then
 			local content = file:read("*a")
 			io.close(file)
 			local data = json.decode(content)
--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,19 +1,35 @@
 return {
-	"voldikss/vim-floaterm",
-	config = function()
-		local keymap = vim.keymap
+  "voldikss/vim-floaterm",
+  config = function()
+    local json = require("lunajson")
+    local keymap = vim.keymap
 
-		keymap.set(
-			"n",
-			"<leader>lt",
-			"<CMD>FloatermNew --autoclose=2 --width=0.9 --height=0.9 nu<CR>",
-			{ desc = "Open terminal" }
-		)
-		keymap.set(
-			"n",
-			"<leader>lf",
-			"<CMD>FloatermNew --autoclose=2 --width=0.9 --height=0.9 spf<CR>",
-			{ desc = "Open superfile" }
-		)
-	end,
+    local settings = {
+      shell = "sh",
+      file_manager = "none",
+    }
+
+    local config_dir = os.getenv("XDG_CONFIG_HOME")
+    local file = io.open(config_dir .. "/strix_vyxlor/env.json", "r")
+    if file then
+      local content = file:read("*a")
+      io.close(file)
+      settings = json.decode(content)
+    end
+
+    keymap.set(
+      "n",
+      "<leader>lt",
+      "<CMD>FloatermNew --autoclose=2 --width=0.9 --height=0.9 " .. settings.shell .. "<CR>",
+      { desc = "Open terminal" }
+    )
+    if settings.file_manager != "none" then
+      keymap.set(
+        "n",
+        "<leader>lf",
+        "<CMD>FloatermNew --autoclose=2 --width=0.9 --height=0.9 " .. settings.file_manager .. "<CR>",
+        { desc = "Open file manager" }
+      )
+    end
+  end,
 }
