5f7d8dae78c6c80dc7858baf88153ed87db89315
Strix Vyxlor <strix.vyxlor@gmail.com>
2025-05-19 10:23:38 +0200

    add more comments

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,51 +1,56 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    local nvimtree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    nvimtree.setup({
-      view = {
-        width = 35,
-        relativenumber = true,
-      },
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
 
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "",
-              arrow_open = "",
-            },
-          },
-        },
-      },
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "",
+							arrow_open = "",
+						},
+					},
+				},
+			},
 
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = true,
-          },
-        },
-      },
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = true,
+					},
+				},
+			},
 
-      git = {
-        ignore = false,
-      },
-    })
+			git = {
+				ignore = false,
+			},
+		})
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>et", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file exporer on current search" })
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
-    keymap.set("n", "<leader>es", "<cmd>NvimTreeFocus<CR>", { desc = "focus on file explorer" })
-  end
+		keymap.set("n", "<leader>et", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		keymap.set(
+			"n",
+			"<leader>ef",
+			"<cmd>NvimTreeFindFileToggle<CR>",
+			{ desc = "Toggle file exporer on current search" }
+		)
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+		keymap.set("n", "<leader>es", "<cmd>NvimTreeFocus<CR>", { desc = "focus on file explorer" })
+	end,
 }
--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -45,6 +45,7 @@ return {
 				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
 				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
 				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+				SECTION = { icon = "", alt = { "PART" } },
 			},
 			gui_style = {
 				fg = "NONE", -- The gui style to use for the fg highlight group.
