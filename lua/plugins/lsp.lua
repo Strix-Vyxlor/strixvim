bdfc3813fbb505ba8d340b20f2c73de06c224ec4
Strix Vyxlor <strix.vyxlor@gmail.com>
2025-06-13 14:41:20 +0200

    signs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -74,6 +74,12 @@ return {
 				},
 			},
 		},
+		sign_text = {
+			error = "✘",
+			warn = "▲",
+			hint = "⚑",
+			info = "»",
+		},
 	},
 	config = function(_, opts)
 		local lspconfig = require("lspconfig")
@@ -171,219 +177,34 @@ return {
 				conform.format({ bufnr = args.buf })
 			end,
 		})
+
+		-- SECTION: icons
+		require("trouble").setup({})
+		local ds = vim.diagnostic.severity
+		local levels = {
+			[ds.ERROR] = "error",
+			[ds.WARN] = "warn",
+			[ds.INFO] = "info",
+			[ds.HINT] = "hint",
+		}
+
+		local text = {}
+
+		for i, l in pairs(levels) do
+			if type(opts.sign_text[l]) == "string" then
+				text[i] = opts.sign_text[l]
+			end
+		end
+
+		vim.diagnostic.config({
+			signs = { text = text },
+			virtual_text = {
+				prefix = "•",
+			},
+			severity_sort = true,
+			float = {
+				source = "always",
+			},
+		})
 	end,
 }
-
--- return {
--- 	"VonHeikemen/lsp-zero.nvim",
--- 	branch = "v4.x",
--- 	dependencies = {
--- 		-- LSP Support
--- 		{ "neovim/nvim-lspconfig" },
---
--- 		-- Autocompletion
--- 		{ "hrsh7th/nvim-cmp" },
--- 		{ "hrsh7th/cmp-nvim-lsp" },
--- 		{ "hrsh7th/cmp-buffer" },
--- 		{ "hrsh7th/cmp-path" },
--- 		{ "saadparwaiz1/cmp_luasnip" },
--- 		{ "hrsh7th/cmp-nvim-lua" },
--- 		{ "onsails/lspkind.nvim" },
--- 		{ "folke/neodev.nvim" },
---
--- 		-- Linting
--- 		{ "mfussenegger/nvim-lint" },
---
--- 		-- Snippets
--- 		{ "L3MON4D3/LuaSnip" },
--- 		{ "rafamadriz/friendly-snippets" },
---
--- 		-- Esthetic
--- 		{ "folke/trouble.nvim" },
--- 		{ "lukas-reineke/lsp-format.nvim" },
---
--- 		-- Misc
--- 		{
--- 			"stevearc/conform.nvim",
--- 		},
---
--- 		-- Rust
--- 		{ "simrat39/rust-tools.nvim" },
--- 		{
--- 			"saecki/crates.nvim",
--- 			tag = "stable",
--- 			dependencies = { "nvim-lua/plenary.nvim" },
--- 			config = function()
--- 				require("crates").setup({
--- 					completion = {
--- 						cmp = {
--- 							enabled = true,
--- 						},
--- 					},
--- 				})
--- 			end,
--- 		},
---
--- 		-- Java
--- 		{
--- 			"nvim-java/nvim-java",
--- 			dependencies = {
--- 				"nvim-java/lua-async-await",
--- 				"nvim-java/nvim-java-refactor",
--- 				"nvim-java/nvim-java-core",
--- 				"nvim-java/nvim-java-test",
--- 				"nvim-java/nvim-java-dap",
--- 				"MunifTanjim/nui.nvim",
--- 				"neovim/nvim-lspconfig",
--- 				"mfussenegger/nvim-dap",
--- 				{
--- 					"JavaHello/spring-boot.nvim",
--- 					commit = "218c0c26c14d99feca778e4d13f5ec3e8b1b60f0",
--- 				},
--- 			},
--- 		},
--- 	},
--- 	config = function()
--- 		local lsp = require("lsp-zero")
---
--- 		require("neodev").setup({})
--- 		require("java").setup({
--- 			jdk = { auto_install = false },
--- 		})
---
--- 		lsp.ui({
--- 			float_border = "rounded",
--- 			sign_text = {
--- 				error = "✘",
--- 				warn = "▲",
--- 				hint = "⚑",
--- 				info = "»",
--- 			},
--- 		})
---
--- 		-- Configure Servers
--- 		lsp.setup_servers({
--- 			"lua_ls",
--- 			"rust_analyzer",
--- 			"zls",
--- 			"yamlls",
--- 			"jsonls",
--- 			"pyright",
--- 			"clangd",
--- 			"ts_ls",
--- 			"jdtls",
--- 		})
---
--- 		require("lspconfig").nil_ls.setup({
--- 			settings = {
--- 				["nil"] = {
--- 					nix = {
--- 						maxMemoryMB = 7680,
--- 						flake = {
--- 							autoArchive = false,
--- 							autoEvalInputs = false,
--- 						},
--- 					},
--- 				},
--- 			},
--- 		})
---
--- 		lsp.on_attach(function(client, _)
--- 			require("lsp-format").on_attach(client)
--- 			vim.keymap.set("n", "<space>ma", function()
--- 				vim.lsp.buf.code_action({ apply = true })
--- 			end, bufopts)
--- 		end)
--- 		lsp.setup()
---
--- 		local cmp = require("cmp")
--- 		local cmp_action = require("lsp-zero").cmp_action()
---
--- 		vim.api.nvim_create_autocmd("BufRead", {
--- 			group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
--- 			pattern = "Cargo.toml",
--- 			callback = function()
--- 				cmp.setup.buffer({ sources = { { name = "crates" } } })
--- 			end,
--- 		})
---
--- 		local opts = { silent = true }
--- 		vim.keymap.set("n", "<leader>mc", require("crates").show_popup, opts)
---
--- 		local luasnip = require("luasnip")
---
--- 		cmp.setup({
--- 			mapping = {
--- 				["<Tab>"] = cmp_action.luasnip_supertab(),
--- 				["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
--- 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
--- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
--- 				["<C-q>"] = cmp.mapping.complete(), -- show completion suggestions
--- 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
--- 				["<CR>"] = cmp.mapping.confirm({ select = true }),
--- 			},
--- 			window = {
--- 				completion = {
--- 					winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
--- 					col_offset = -3,
--- 					side_padding = 0,
--- 				},
--- 			},
--- 			sources = cmp.config.sources({
--- 				{ name = "nvim_lsp" },
--- 				{ name = "luasnip" }, -- snippets
--- 				{ name = "buffer" }, -- text within current buffer
--- 				{ name = "path" }, -- file system paths
--- 				{ name = "neorg" },
--- 				{ name = "crates" },
--- 			}),
--- 			formatting = {
--- 				fields = { "kind", "abbr", "menu" },
--- 				format = function(entry, vim_item)
--- 					local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
--- 					local strings = vim.split(kind.kind, "%s", { trimempty = true })
--- 					kind.kind = " " .. (strings[1] or "") .. " "
--- 					kind.menu = "    (" .. (strings[2] or "") .. ")"
---
--- 					return kind
--- 				end,
--- 			},
--- 		})
---
--- 		vim.opt.signcolumn = "yes" -- Disable lsp signals shifting buffer
---
--- 		vim.diagnostic.config({
--- 			virtual_text = true,
--- 		})
---
--- 		require("conform").setup({
--- 			formatters_by_ft = {
--- 				lua = { "stylua" },
--- 				nix = { "alejandra" },
--- 				rust = { "rustfmt" },
--- 			},
--- 			format_on_save = {
--- 				lsp_fallback = true,
--- 				async = false,
--- 				timeout_md = 500,
--- 			},
--- 		})
---
--- 		vim.api.nvim_create_autocmd("BufWritePre", {
--- 			pattern = "*",
--- 			callback = function(args)
--- 				require("conform").format({ bufnr = args.buf })
--- 			end,
--- 		})
---
--- 		-- lint
--- 		require("lint").linters_by_ft = {
--- 			nix = { "statix" },
--- 		}
--- 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
--- 			callback = function()
--- 				require("lint").try_lint()
--- 			end,
--- 		})
--- 	end,
--- }
